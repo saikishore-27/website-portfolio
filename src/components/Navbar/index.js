ba72b52878cc0a6a699bbc7e35c0acab821ea863
saikishore-27 <157101743+saikishore-27@users.noreply.github.com>
2024-11-08 18:04:11 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -51,7 +51,7 @@ const Navbar = () => {
             <MobileLink href='#education' onClick={() => {
               setIsOpen(!isOpen)
             }}>Education</MobileLink>
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Portfolio</GitHubButton>
           </MobileMenu>
         }
       </NavbarContainer>
